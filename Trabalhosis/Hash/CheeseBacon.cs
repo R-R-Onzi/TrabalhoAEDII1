09aa65d7cd514b18b8b684097db9dc8d75f4f37c
R-R-Onzi <56856330+R-R-Onzi@users.noreply.github.com>
2019-10-30 16:08:56 -0300

    Add files via upload

--- Trabalhosis/Hash/CheeseBacon.cs
+++ Trabalhosis/Hash/CheeseBacon.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Trabalhosis
@@ -10,7 +13,22 @@ namespace Trabalhosis
     {
         public void Run()
         {
-            FazHash.HashArk();
+            Hashtable ind;
+            ind = FazHash.HashArk();
+            IndiceHash indice;
+            Console.WriteLine("Digite a data");
+            string aux3 = Console.ReadLine();
+            aux3= Regex.Replace(aux3, @"[\D]", "");
+            long esse = Convert.ToInt64(aux3);
+            indice = (IndiceHash)ind[esse];
+            FileStream le = new FileStream(Environment.CurrentDirectory + @"\main.dat", FileMode.Open);
+            le.Seek(indice.pos,0);
+           
+            byte[] po = new byte[2000];
+            le.Read(po, 0, 620);
+            aux3 = new string(Encoding.UTF8.GetChars(po));
+            Console.WriteLine(aux3);
+            Console.ReadKey();
         }
     }
 }
--- Trabalhosis/Hash/FazHash.cs
+++ Trabalhosis/Hash/FazHash.cs
@@ -4,19 +4,22 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using System.Collections;
 
 namespace Trabalhosis
 {
     class FazHash
     {
-        public static void HashArk()
+        public static Hashtable HashArk()
         {
-         
-            DateTime now;
-            
-            BinaryWriter a = new BinaryWriter(File.OpenWrite(Environment.CurrentDirectory + @"\Hash.dat"));
+
+
+
+
+            Hashtable hashtable = new Hashtable();
             FileStream le = new FileStream(Environment.CurrentDirectory + @"\main.dat",FileMode.Open);
-            HashSet<IndiceHash> ind = new HashSet<IndiceHash>();
+            IndiceHash ind;
             byte[] po =new byte[2000];
             IndiceHash indiceHash;
             string aux,fim;
@@ -24,29 +27,35 @@ namespace Trabalhosis
             int end = 0;
               do
             {
-
-                le.Read(po, 0, 641);
+                
+                le.Read(po, 0, 620);
                 aux = new string(Encoding.UTF8.GetChars(po));
-                fim = aux.Substring(21,20)+"fim";
-                /* try
-                 {
-                     now = Convert.ToDateTime(fim);
-                     indiceHash = new IndiceHash(now,)
-                     ind.Add();
-                 }
-                 catch (Exception)
-                 {
+                fim = aux.Substring(506,10);
+                fim = Regex.Replace(fim, @"[\D]", "");
+                
+                try
+                {
 
-                     throw;
-                 }*/
-                Console.WriteLine(fim);
-                Console.ReadKey();
+                    int ava = Convert.ToInt32(fim);
+                    uint ia = Convert.ToUInt32(ava);
+                    uint thy = ia;
+                    
+                    indiceHash = new IndiceHash(ava,620*end);
+                    hashtable.Add(ia,indiceHash);
+                    
+                    
+                }
+                catch (Exception)
+                {
+
+
+                }
                 end++;
                 
             } while (le.CanRead);
 
             le.Close();
-            
+            return hashtable;
         }
     }
 }
