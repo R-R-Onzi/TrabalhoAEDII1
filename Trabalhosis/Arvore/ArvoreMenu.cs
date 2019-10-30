b887565e9d74b0f8178387041fecf976140127a8
HackIssue5560 <57115645+HackIssue5560@users.noreply.github.com>
2019-10-29 23:53:17 -0300

    Add files via upload

---
+++ Trabalhosis/Arvore/ArvoreMenu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Trabalhosis
+{
+    class ArvoreMenu : IMenuAction
+    {
+        public void Run()
+        {
+            ArvoreBin tree = new ArvoreBin();
+            int op = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("------------------MENU--------------------\n");
+                Console.WriteLine("1- Criar arvore.");
+                Console.WriteLine("2- Buscar.");
+                Console.WriteLine("3- Escrever arvore.");
+                Console.WriteLine("0- Sair ");
+                Console.WriteLine("Digite sua opcao: ");
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        byte[] hashtags = new byte[2000];
+                        string aux, fim;
+
+                        long end = 0;
+
+                        FileStream le = new FileStream(Environment.CurrentDirectory + @"\main.dat", FileMode.Open);
+                        do
+                        {
+                            le.Read(hashtags, 0, 620);
+                            aux = new string(Encoding.UTF8.GetChars(hashtags));
+                            fim = aux.Substring(517, 100);
+                            string[] hashstring = fim.Split('#');
+                            foreach (var item in hashstring)
+                            {
+                                tree.Inserir(item, end);
+                            }
+
+                            end+=620;
+                        } while (le.CanRead);
+                        le.Close();
+                        break;
+                    case 2:
+                        Console.WriteLine("Informe a hashtag: ");
+                        string hashtag = Console.ReadLine();
+                        tree.Busca(tree.Root, hashtag);
+                        break;
+                    case 3:
+                        tree.Imprimir(tree.Root);
+                        break;
+                }
+            } while (op != 0);
+        }
+    }
+}
