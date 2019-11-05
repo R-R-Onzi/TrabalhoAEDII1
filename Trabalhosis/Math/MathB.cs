93f788b10d444e6664dfdfe76094628209d9cb23
R-R-Onzi <56856330+R-R-Onzi@users.noreply.github.com>
2019-11-05 18:40:37 -0300

    Add files via upload

---
+++ Trabalhosis/Math/MathB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalhosis
+{
+    class MathB
+    {
+        public static int Separator(int a,int lenght)
+        {
+            int aux=0;
+            int f;
+            int j=1;
+            for (int i =0;i<lenght;i++,j*=10)
+            {
+                f = a % 10;
+                a /= 10;
+                aux += f * j;
+            }
+
+
+
+            return aux;
+        }
+        public static int Hash(int a) 
+        {
+            int ini = a % 10000;
+            a /= 10000;
+            ini %= 2000;
+            a %= 12;
+           
+            
+            return ini * 12 + a;
+        }
+    }
+
+    
+}
